887b034cef09734d8caa59761aeffe3b9cbb39cb
Nicolebalzano <nicole.balzano03@gmail.com>
2025-06-17 14:08:01 +0200

    feat:ricerca allargata per le serie tv

--- src/AppHome.jsx
+++ src/AppHome.jsx
@@ -3,16 +3,27 @@ import { useState, useEffect } from "react"
 const AppHome = () => {
     const apiKey = import.meta.env.VITE_API_KEY;
     const [search, setSearch] = useState("");
-    const [data, setData] = useState([]);
+    const [dataMovie, setDataMovie] = useState([]);
+      const [dataSeries, setDataSeries] = useState([]);
      const fetchMovies =() => {
-            axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${search}`)
-        .then((resp) => {
-            const dataList = resp.data.results;
-            setData(dataList)})
+        if (!search.trim()) return;
+        Promise.all([ axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${search}`),
+            axios.get(`https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&query=${search}`)
+        ]).then(([response1, response2]) => {
+             const dataMovieList = response1.data.results;
+            setDataMovie(dataMovieList)
+            const dataSeriesList = response2.data.results;
+            setDataSeries(dataSeriesList)})
         }
+           
+      
     useEffect(() => {
-       fetchMovies()
+        if(search.trim()){
+             fetchMovies()
+        }
+      
     }, [])
+    
    
 return (
     <>
@@ -20,10 +31,10 @@ return (
     value={search}
     onChange={(e) =>setSearch(e.target.value)}/>
     <div>    <button onClick={fetchMovies}>Cerca</button>
-{data.map((curData, index) => (
+{[...dataSeries, ...dataMovie].map((curData, index) => (
     <div key={index}>
-            <h3 >Titolo:{curData.title}</h3>
-            <h4>Titolo Originale:{curData.original_title}</h4>
+            <h3 >Titolo:{curData.title} || {curData.name}</h3>
+            <h4>Titolo Originale:{curData.original_title} || {curData.original_name}</h4>
 <h5>
   Lingua Originale: {curData.original_language === "en" ? (
     <img src="american-flag.jpeg" className="lang-flag" alt="Bandiera Americana" />
